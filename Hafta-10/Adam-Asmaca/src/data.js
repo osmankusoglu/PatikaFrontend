bedad0cd5b51b48312469a5b06c0dae40431ad15
Osman Kuşoğlu <kusogluo@gmail.com>
2024-05-28 06:13:26 +0300

    Adam Asmaca

---
+++ Hafta-10/Adam-Asmaca/src/data.js
@@ -0,0 +1,5 @@
+export const DATA = [
+    {question: 'Bir Hayvan adı ?', answer: 'Eşşek'},
+    {question: 'Bir Balık adı ?', answer: 'Kefal'},
+    {question: 'Bir Renk adı ?', answer: 'Mavi'},
+]
